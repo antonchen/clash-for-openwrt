0c8c6816cf5107d1bd223390962c29fd3a4e9cb7
Anton Chen <contact@antonchen.com>
2019-05-30 21:59:08 +0800

    exit immediately if a error

--- luci-app-clash/files/usr/share/clash/update-mmdb.sh
+++ luci-app-clash/files/usr/share/clash/update-mmdb.sh
@@ -1,8 +1,9 @@
 #!/bin/ash
 # Author: Anton Chen <contact@antonchen.com>
 # Create Date: 2019-05-07 11:26:42
-# Last Modified: 2019-05-23 14:03:39
+# Last Modified: 2019-05-30 21:55:58
 # Description: 
+set -e
 rm -rf /tmp/GeoLite2-Country*
 curl -s -k --connect-timeout 30 -m 30 -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36" http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz -o /tmp/GeoLite2-Country.tar.gz
 cd /tmp
